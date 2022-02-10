d765f828f754433c424fd9e6f98058e7dc4c829a
Nirav <98092147+niravmp@users.noreply.github.com>
2022-02-10 10:44:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+// console.log('Hello Word');
+
+// //variable
+// let name ='Nirav';
+// console.log(name);
+
+// //constant
+// const x= 1;
+// console.log(x);
+
+// Primitive Types 
+
+// let name = 'nirav'//string litral
+// let age = 30;//Number
+// let isApproved = false//bool
+// let firstName = undefined;
+// let lastname = null;
+
+// object code
+
+
+// let person ={
+//     name:'Nirav',
+//     age: 30
+// };
+
+// //accessing objects.
+// //1.Dot Notation Method
+// person.name ='John';
+// console.log(person.name);
+
+// ///2.Bracket Notation Method
+// person['name']='Mary';
+// console.log(person.name);
+
+// //Arrays In Java Script
+
+// let selectColors = ['red','blue','green'];
+
+// console.log(selectColors.length);
+
+// //Function in javascript
+
+// function greet(name,lastname){
+//     console.log('Hello ' + name+' '+lastname);
+// }
+
+// greet('John','smith');
+
+// //Function That Calculate Value.
+// function square(number)
+// {
+//     return number*number;
+// }
+
+// let number = square(2);
+// console.log(number);
