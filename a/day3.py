a2d2953b9b5502b9499bd378f3b038f6dbcd1743
niravmp <nirav.patel12@einfochips.com>
2022-03-09 18:41:51 +0530

    FLAMES

--- a/day3.py
+++ a/day3.py
@@ -84,3 +84,38 @@ c2=[5,6,7,8]
 #IN OR NOT IN
 #3 in c1
 #3 not in c1
+def Flames(name1,name2):
+    namestr = name1 + name2
+
+    for c in namestr:
+        if namestr.count(c) != 1:
+          
+            namestr = namestr.replace(c,"")
+
+    print("FLAMES....")
+    print("F = Friend \nL = Love \nA = Affection \nM = Marriage \nE = Enemy \nS = Siblings \n\n")
+    
+
+    number = len(namestr) % 6
+    rel = ""
+
+    if number == 1:
+        rel += "Friends"
+    elif number == 2:
+        rel += "Love"
+    elif number == 3:
+        rel += "Affection"
+    elif number == 4:
+        rel += "Marriage"
+    elif number == 5:
+        rel += "Enemy"
+    elif number == 0:
+        rel += "Siblings"
+    else:
+        pass
+
+    return rel
+
+n1 = input("Enter your name : ").upper()
+n2 = input("Enter name of your crush : ").upper()
+print(f"Your Relationship is : {Flames(n1,n2)}")
