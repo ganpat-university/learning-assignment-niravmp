85b561f3eb80b1d9dfcd94035653dd24d172c570
niravmp <nirav.patel12@einfochips.com>
2022-03-09 18:27:06 +0530

    day3_python

---
+++ a/day3.py
@@ -0,0 +1,86 @@
+id = [6,7,8,9]
+emp_name=["abc","pqr","xyz"]
+student_detail=[1,"abc",25.3,id]
+
+#insert element in list
+#id.append(100)
+#id.insert(4,100)
+#id.insert(5,700)
+#id.insert(2,200)
+
+
+#print(id)
+#print(emp_name)
+#print(student_detail)
+#id
+
+
+#index slicing 
+
+#id[1:4]
+#id[0:0]
+#id[::3]
+
+#delete = index
+#remove = value
+#pop = return the element(same as delete)
+
+
+#del id[2]
+#id
+
+#id.pop(3)
+#id
+
+#  SORT,REVERSE,LENGTH
+
+#lst = [3,5,100,2,7,9]
+#lst1 = ['ab','ww','ee','rr']
+#lst.sort()
+#lst1.sort()
+#lst
+#lst1
+
+#lst1 = [3,5,100,2,7,9]
+#lst2 = [2,5,100,2,7,9]
+#lst3 = [1,5,100,2,7,9]
+
+#lst=[lst1,lst2,lst3]
+#lst.sort()
+
+#for i in lst:
+
+# i.sort()
+  
+#lst
+
+#student_detail.reverse()
+#student_detail
+
+#PRINTING A LENGTH
+#len(id)
+
+#COUNT ELEMENT
+#count=[2,2,4,4,6,6]
+#count.count(4)
+
+#RECOGNIZING THE INDEX
+#id.index(6)
+
+#MAXIMUM NUMBER
+#max(id)
+#min(id)
+
+#FIND THE SUM OF LIST
+#sum(id)
+
+#APPEND LIST1 > LIST2
+c1=[1,2,3,4]
+c2=[5,6,7,8]
+#c1.extend(c2)
+#c1+c2
+#c1
+
+#IN OR NOT IN
+#3 in c1
+#3 not in c1
